f5a7b9fa61175ac757b1e38d15c5429ff33a0d54
kabirseth4 <kabirseth4@gmail.com>
2024-01-25 11:43:35 +0000

    Add 3D render canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import Render from "./components/Render/Render";
 import Sketcher from "./components/Sketcher/Sketcher";
 
 const App = () => {
   return (
     <main>
+      <Render />
       <Sketcher />
     </main>
   );
--- src/components/Sketcher/Sketcher.jsx
+++ src/components/Sketcher/Sketcher.jsx
@@ -1,33 +1,31 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import ChemDoodle from "../../libraries/ChemDoodleWeb/uis/ChemDoodleWeb-uis";
 import "../../libraries/ChemDoodleWeb/ChemDoodleWeb.css";
 import "../../libraries/ChemDoodleWeb/uis/jquery-ui-1.11.4.css";
 import "./Sketcher.scss";
 
 const Sketcher = () => {
-  const sketcher = useRef(null);
-
   useEffect(() => {
     ChemDoodle.ELEMENT["H"].jmolColor = "black";
     ChemDoodle.ELEMENT["S"].jmolColor = "#B9A130";
 
-    sketcher.current = new ChemDoodle.SketcherCanvas("sketcher", 375, 300, {
+    const sketcher = new ChemDoodle.SketcherCanvas("sketcher", 390, 300, {
       useServices: false,
       oneMolecule: true,
       isMobile: true,
     });
-    sketcher.current.styles.atoms_displayTerminalCarbonLabels_2D = true;
-    sketcher.current.styles.atoms_useJMOLColors = true;
-    sketcher.current.styles.bonds_clearOverlaps_2D = true;
-    sketcher.current.styles.shapes_color = "c10000";
-    sketcher.current.repaint();
-    sketcher.current.toolbarManager.setup();
+    sketcher.styles.atoms_displayTerminalCarbonLabels_2D = true;
+    sketcher.styles.atoms_useJMOLColors = true;
+    sketcher.styles.bonds_clearOverlaps_2D = true;
+    sketcher.styles.shapes_color = "c10000";
+    sketcher.repaint();
+    sketcher.toolbarManager.setup();
   }, []);
 
   return (
-    <div className="sketcher">
-      <canvas id="sketcher"></canvas>
-    </div>
+    <article className="sketcher">
+      <canvas id="sketcher" />
+    </article>
   );
 };
 
